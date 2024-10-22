3a83a8ca51b2db2e36813f0639bde8799a4d3f1f
Kristaps <kbebris+git@gmail.com>
2024-10-22 17:08:57 +0300

    dealing with log entries that don't use modifiers

--- parse_logs.py
+++ parse_logs.py
@@ -9,7 +9,7 @@ with open("server.log", "r") as f:
             coords.append([
                 datetime.strptime(line.split(",")[0][1:], '%Y-%m-%d %H:%M:%S'), # time
                 float(line.split("=")[1].split("&")[0]), # lat
-                float(line.split("=")[2].split("&")[0]) # lon
+                float(line.split("=")[2].split("&")[0].split(" ")[0]) # lon
             ])
 
 #print(coords)
