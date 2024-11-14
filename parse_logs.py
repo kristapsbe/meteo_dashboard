a9b00b1b7ef7f7add1abd540f012479e2de251ac
Kristaps <kbebris+git@gmail.com>
2024-11-14 22:51:34 +0200

    updated visual

--- parse_logs.py
+++ parse_logs.py
@@ -15,9 +15,10 @@ with open("server.log", "r") as f:
                 #datetime.strptime(line.split(",")[0][1:], '%Y-%m-%d %H:%M:%S'), # time
             ])
 
-df = pd.DataFrame([e.split(",") for e in list(set([",".join(c) for c in coords]))])
+unique_coords = [e.split(",") for e in list(set([",".join(c) for c in coords]))]
+df = pd.DataFrame(unique_coords)
 #print(df)
-coordstr = ','.join([f"new google.maps.LatLng({c[0]}, {c[1]})" for c in coords])
+coordstr = ','.join([f"new google.maps.LatLng({c[0]}, {c[1]})" for c in unique_coords])
 
 with open("map.js", "w") as f:
     f.write("".join(open("map.temp.js", "r").readlines()).replace("<latlng>", coordstr))
